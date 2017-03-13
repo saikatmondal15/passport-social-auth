915517336a4c94bd955aab36ff24378cec04ae1b
souparno majumder <souparno.majumder@gmail.com>
2017-03-13 08:58:26 -0700

    config fiile added

---
+++ server/_config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  github: {
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  linkedin: {
+    clientID: 'get_your_own',
+    clientSecret: 'get_your_own',
+    callbackURL: "http://127.0.0.1:3000/auth/linkedin/callback"
+  },
+  facebook: {
+    clientID: '373995389622731',
+    clientSecret: '415f14a17a82f37026cf613501f892ca',
+    callbackURL: "http://cuboid.io/auth/facebook/callback"
+  },
+  google: {
+    clientID: '721383437750-gkpam2jrasavf8qduj5e54kn1l094ri9.apps.googleusercontent.com',
+    clientSecret: 'xsHhfOd_-paFPksucFrXwmbi',
+    callbackURL: "http://cuboid.io/auth/google/callback"
+  },
+  twitter: {
+    consumerKey: 'AmJMqcNknaL7SdrtujTHjwt2X',
+    consumerSecret:'G2bhMwZcXQGxALIzHeVAA0jcPrC7sjKbA5irpfJuCbvW3QFqfx',
+    callbackURL: "http://cuboid.io/auth/twitter/callback"
+  }
+}; 
